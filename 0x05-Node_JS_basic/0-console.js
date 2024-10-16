9d0d90a5dc4792dbd7a89bce7d4c4a0dafc62f6d
Plantinah99 <plantinahtshukudu@gmail.com>
2024-10-16 13:36:44 -0700

    Node_JS basics

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Plantinah Tshukudu <https://github.com/Plantinah99>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
